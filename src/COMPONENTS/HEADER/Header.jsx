e62f8efce02105595c5fda14b20a20552d7524fa
msetitech <msetitech@yahoo.com>
2025-01-20 11:45:25 +0300

    Complete header

--- src/COMPONENTS/HEADER/Header.jsx
+++ src/COMPONENTS/HEADER/Header.jsx
@@ -5,7 +5,48 @@ export default function Header() {
   return (
     <div>
       <header>
-        <img src="images/background.png" alt="" />
+        <div className="imageHeader">
+          <img src="images/background.png" alt="" />
+        </div>
+        <div className="conteined">
+          <div className="leftcontentHeader">
+            <h1>
+              INTER MIAMI <br /> FOOTBALL CLUB{" "}
+            </h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio ad
+              sapiente architecto vel laudantium error beatae sed nobis repellat
+              in fugit voluptates
+            </p>
+            <button>Read More</button>
+          </div>
+          <div className="rightContentHeader">
+            <div className="next">
+              <h1>
+                NEXT MATCH <br />
+                vs ARSENAL
+              </h1>
+            </div>
+            <div className="leftDay">
+              <div className="Day-Min-Sec">
+                <h1>21</h1>
+                <p>Days</p>
+              </div>
+              <div className="Day-Min-Sec">
+                <h1>10</h1>
+                <p>Hrs</p>
+              </div>
+              <div className="Day-Min-Sec">
+                <h1>55</h1>
+                <p>Min</p>
+              </div>
+              <div className="Day-Min-Sec">
+                <h1>21</h1>
+                <p>Secs</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
