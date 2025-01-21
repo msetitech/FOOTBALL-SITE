f629b5da610647378c3c71d56a56d402406eaba5
msetitech <msetitech@yahoo.com>
2025-01-21 21:02:28 +0300

    Complete design Card Pop UP

--- src/COMPONENTS/TEAMLEGEND/TeamLegendPopUP.jsx
+++ src/COMPONENTS/TEAMLEGEND/TeamLegendPopUP.jsx
@@ -16,6 +16,21 @@ export default function TeamLegendPopUP({ poplegend }) {
               <h1>{legend.name}</h1>
               <h4>{legend.position}</h4>
               <p>{legend.description}</p>
+              <div className="statusOnTeam">
+                <div className="statusSingle">
+                  <h2>{legend.match}</h2>
+                  <h4>Matches</h4>
+                </div>
+                <div className="statusSingle">
+                  <h2>{legend.goal}</h2>
+                  <h4>Goal</h4>
+                </div>
+
+                <div className="statusSingle">
+                  <h2>{legend.yelloCard}</h2>
+                  <h4>Yellow Card</h4>
+                </div>
+              </div>
             </div>
           </div>
         ))}
